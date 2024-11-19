d6e9e23abbd3d51380e90a4da81d9fe61b164c28
William Chong <me@williamchong.cloud>
2024-11-19 16:57:32 +0800

    🔍️ Add proper title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Bech32 Wallet Address Prefix Web Converter',
+      meta: [
+        { name: 'og:title', content: 'Bech32 Wallet Address Prefix Web Converter' },
+        { name: 'description', content: 'Convert between Cosmos(cosmos1), Ethereum(0x) and different cosmos-based chain using bech32 address prefixes.' }
+      ],
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   experimental: {
