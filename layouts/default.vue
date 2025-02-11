0b60a3ef2641fbd9cd4d04a937a765c63eb69e73
William Chong <me@williamchong.cloud>
2025-02-11 17:04:20 +0800

    ♻️ Refactor app.vue to pages

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+<script setup lang='ts'>
+useHead({
+  script: [
+    {
+      type: 'application/ld+json',
+      children: JSON.stringify({
+        '@context': 'https://schema.org',
+        '@type': 'WebApplication',
+        name: 'Bech32 Wallet Address Prefix Web Converter',
+        description: 'Convert between Cosmos(cosmos1), Ethereum(0x) and different cosmos-based chain using bech32 address prefixes.',
+        url: 'https://bech32.williamchong.cloud',
+        image: 'https://bech32.williamchong.cloud/images/cover.png',
+        applicationCategory: 'DeveloperApplication',
+        author: {
+          '@type': 'Person',
+          name: 'William Chong',
+          url: 'https://blog.williamchong.cloud'
+        },
+        offers: {
+          '@type': 'Offer',
+          price: '0',
+          priceCurrency: 'USD'
+        },
+      })
+    }
+  ]
+})
+
+useSeoMeta({
+  title: 'Bech32 Wallet Address Prefix Web Converter',
+  ogTitle: 'Bech32 Wallet Address Prefix Web Converter',
+  description: 'Convert between Cosmos(cosmos1), Ethereum(0x) and different cosmos-based chain using bech32 address prefixes.',
+  ogDescription: 'Convert between Cosmos(cosmos1), Ethereum(0x) and different cosmos-based chain using bech32 address prefixes.',
+  ogImage: 'https://bech32.williamchong.cloud/images/cover.png',
+  ogType: 'website',
+  ogUrl: 'https://bech32.williamchong.cloud',
+})
+</script>
